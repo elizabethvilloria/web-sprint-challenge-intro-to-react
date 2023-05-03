485582baaccef4c6e8f59b44135789cd60adbd63
elizabethvilloria <lizvilloria@gmail.com>
2023-05-03 19:08:28 -0400

    Finished mvp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+
+import Character from './components/Character';
+
+const CharacterListWrapper = styled.div`
+  align-items:center;
+  h1 {
+    font-size: 80px;
+    color: #F5F5DC;
+  }
+
+
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +23,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setCharacters(res.data);
+      console.log(res.data)
+      console.log({characters})
+    })
+    .catch(err => console.log(err))
+  }, [])
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <CharacterListWrapper className="App">
+      <h1>STAR WARS CHARACTERS</h1>
+      {characters.map(character => (
+        <Character key={character.url} character={character} />
+      ))}
+    </CharacterListWrapper>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+
 // Write your Character component here
+
+const CharacterStyle = styled.div`
+
+
+h1{
+    font-size: 50px;
+}
+
+h3 {
+    color: #A52A2A;
+}
+
+.bio {
+    background-color: white;
+    opacity: .4;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center
+
+    
+}
+
+.bio-details {
+    display: flex;
+    flex-direction: column;
+    
+
+}
+
+`
+
+const Character = ({character}) => {
+    return (
+        <CharacterStyle>
+            <h1>{character.name}</h1>
+            <div className='bio'>
+                <h2>Bio: </h2>
+                <div className='bio-details'>
+                    <h3>Height: {character.height}</h3>
+                    <h3>Hair Color: {character.hair_color}</h3>
+                </div>
+            </div>
+        </CharacterStyle> 
+    );
+  };
+
+export default Character;
